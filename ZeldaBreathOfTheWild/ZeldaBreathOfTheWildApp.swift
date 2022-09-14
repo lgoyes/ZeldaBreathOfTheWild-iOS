fad6db0849a808d5a483fc13e9cf3f1f697d6059
Luis David Goyes <ldgoyes@veritran.com>
2022-09-13 19:16:43 -0500

    Initial Commit

---
+++ ZeldaBreathOfTheWild/ZeldaBreathOfTheWildApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeldaBreathOfTheWildApp.swift
+//  ZeldaBreathOfTheWild
+//
+//  Created by Luis David Goyes on 13/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ZeldaBreathOfTheWildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
